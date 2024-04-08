7e067864e756dd987996ee973e754b1de63bc3a6
sankusa <91744435+sankusa@users.noreply.github.com>
2024-04-09 04:53:56 +0900

    ビルドエラーを起こす不要なusingディレクティブを削除

--- Assets/TextMeshProEffector/Scripts/Runtime/TypeWriters/RandomTypeWriter.cs
+++ Assets/TextMeshProEffector/Scripts/Runtime/TypeWriters/RandomTypeWriter.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Codice.Client.Commands;
 using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
